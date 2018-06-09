aa829c31daf42316bab7c983d1524d0cff0d3fcd
James Coonce <coonce.james@gmail.com>
2018-06-09 02:10:58 -0400

    Create Auth Service

--- server/todo-api/src/auth/auth.service.ts
+++ server/todo-api/src/auth/auth.service.ts
@@ -1,4 +1,22 @@
+import * as jwt from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService) { }
+
+    async createToken(user) {
+        const expiresIn = 3600;
+        const accessToken = jwt.sign({ id: user.id, email: user.username }, 'secretKey', { expiresIn });
+        return {
+            expiresIn,
+            accessToken,
+        };
+    }
+    async validateUser(payload: JwtPayload): Promise<any> {
+        return await this.usersService.findById(payload.id);
+    }
+}
