4667d11c4b494b8d12ea39c99b20239a16d9187c
James Coonce <coonce.james@gmail.com>
2018-06-09 12:03:29 -0400

    Add User Registration Controller Action

--- server/todo-api/src/auth/auth.controller.ts
+++ server/todo-api/src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, HttpStatus, Response, Request, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/createUser.dto';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiUseTags, ApiResponse } from '@nestjs/swagger';
+import { RegistrationStatus } from './interfaces/registrationStatus.interface';
 
+@ApiUseTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post()
+    public async register(@Response() res, @Body() createUserDto: CreateUserDto){
+        const result = await this.authService.register(createUserDto);
+        if (!result.success){
+            return res.status(HttpStatus.BAD_REQUEST).json(result.message);
+        }
+        return res.status(HttpStatus.OK).json(result.message);
+    }
+
+    @Post()
+    @UseGuards(AuthGuard('login'))
+    public async login(@Response() res, @Body() login){
+        
+    }
+}
--- server/todo-api/src/auth/auth.service.ts
+++ server/todo-api/src/auth/auth.service.ts
@@ -1,12 +1,40 @@
 import * as jwt from 'jsonwebtoken';
-import { sign } from 'jsonwebtoken';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { Model } from 'mongoose';
+import { IUser } from '../users/interfaces/user.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { debug } from 'console';
+import { RegistrationStatus } from './interfaces/registrationStatus.interface';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(private readonly usersService: UsersService,
+                @InjectModel('User') private readonly userModel: Model<IUser>) { }
+
+    async register(user: IUser) {
+        const result: RegistrationStatus = await this.userModel.register(new this.userModel({username: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName}), user.password, (err) => {
+            let status: RegistrationStatus;
+            if (err) {
+                debug(err);
+                status = { success: false, message: err };
+                return status;
+            }
+
+            status = { success: false, message: 'user register' };
+            debug(status.message);
+            return status;
+        });
+
+        return result;
+    }
+
+    async login(){
+
+    }
 
     async createToken(user) {
         const expiresIn = 3600;
