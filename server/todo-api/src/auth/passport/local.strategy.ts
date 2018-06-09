306706575426baafa20e5265f9d7d83e9397dff9
James Coonce <coonce.james@gmail.com>
2018-06-09 02:46:12 -0400

    Add JWT Strategy

---
+++ server/todo-api/src/auth/passport/local.strategy.ts
@@ -0,0 +1,15 @@
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import UserSchema from '../../users/schemas/user.schema';
+
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly authService: AuthService) {
+        super({
+            usernameField: 'email',
+            passwordField: 'password',
+        }, UserSchema.authenticate());
+    }
+}
