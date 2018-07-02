49577109a57830bb350519c55133cdb6606785eb
James Coonce <coonce.james@gmail.com>
2018-07-02 17:59:14 -0400

    Add Ant Design

--- client/todo-client/src/app/app.module.ts
+++ client/todo-client/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import module
-import { ElModule } from 'element-angular';
 
 
 import { AppComponent } from './app.component';
@@ -14,6 +12,13 @@ import { AboutComponent } from './about/about.component';
 import { TodosListComponent } from './todos-list/todos-list.component';
 import { TodoComponent } from './todo/todo.component';
 import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 
 
@@ -41,9 +46,11 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     BrowserModule,
     BrowserAnimationsModule,
-    ElModule.forRoot(),
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
